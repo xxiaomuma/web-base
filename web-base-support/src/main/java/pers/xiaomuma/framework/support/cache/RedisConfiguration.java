fa56d851d73d9a02521579ef0082a16181252a3a
xhy <m18870885528@163.com>
2022-10-11 12:55:06 +0800

    feat redis采用jedis

--- web-base-support/src/main/java/pers/xiaomuma/framework/support/cache/RedisConfiguration.java
+++ web-base-support/src/main/java/pers/xiaomuma/framework/support/cache/RedisConfiguration.java
@@ -1,74 +1,49 @@
 package pers.xiaomuma.framework.support.cache;
 
-import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.time.Duration;
-import java.util.Objects;
+import pers.xiaomuma.framework.serialize.ObjectMapperFactory;
 
 
 @Configuration
 @EnableConfigurationProperties({RedisProperties.class})
-//@Import(LettuceConnectionFactory.class)
+@EnableCaching
 public class RedisConfiguration {
 
     @Bean
-    public LettuceConnectionFactory lettuceConnectionFactory(RedisProperties redisProperties) {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        configuration.setDatabase(redisProperties.getDatabase());
-        configuration.setHostName(redisProperties.getHost());
-        configuration.setPort(redisProperties.getPort());
-        configuration.setPassword(redisProperties.getPassword());
-        RedisProperties.Pool pool = Objects.isNull(redisProperties.getLettuce().getPool()) ? new RedisProperties.Pool() : redisProperties.getLettuce().getPool();
-        GenericObjectPoolConfig<?> poolConfig = new GenericObjectPoolConfig<>();
-        poolConfig.setMaxIdle(pool.getMaxIdle());
-        poolConfig.setMinIdle(pool.getMinIdle());
-        poolConfig.setMaxTotal(pool.getMaxActive());
-        poolConfig.setMaxWaitMillis(pool.getMaxWait().toMillis());
-        Duration timeout = redisProperties.getTimeout();
-        if (Objects.isNull(timeout)) {
-            timeout = Duration.ZERO;
-        }
-        LettucePoolingClientConfiguration.LettuceClientConfigurationBuilder builder =
-                LettucePoolingClientConfiguration.builder()
-                        .poolConfig(poolConfig)
-                        .commandTimeout(timeout)
-                        .shutdownTimeout(timeout);
-        return new LettuceConnectionFactory(configuration, builder.build());
-    }
-
-    @Bean
     @ConditionalOnMissingBean(RedisTemplate.class)
-    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(lettuceConnectionFactory);
-        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
-        template.setValueSerializer(jackson2JsonRedisSerializer);
-        template.setKeySerializer(stringRedisSerializer);
-        template.setHashValueSerializer(jackson2JsonRedisSerializer);
-        template.setHashKeySerializer(stringRedisSerializer);
-        template.afterPropertiesSet();
-        return template;
+    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        jackson2JsonRedisSerializer.setObjectMapper(customObjectMapper());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setConnectionFactory(factory);
+        return redisTemplate;
     }
 
-    @Bean
-    @ConditionalOnMissingBean(StringRedisTemplate.class)
-    public StringRedisTemplate stringRedisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
-        StringRedisTemplate template = new StringRedisTemplate();
-        template.setConnectionFactory(lettuceConnectionFactory);
-        return template;
+    private ObjectMapper customObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        JavaTimeModule javaTimeModule = ObjectMapperFactory.customizedJavaTimeModule();
+        objectMapper.registerModule(javaTimeModule);
+        return objectMapper;
     }
+
 }
