2b80444b78270b63ab4daf7596f263e2a558d6c6
18146695070 <m18870885528@163.com>
2022-12-26 21:44:43 +0800

    fix 七牛云分片上传调整策略

--- web-base-thirdparty/src/main/java/pers/xiaomuma/framework/thirdparty/oss/OSSClient.java
+++ web-base-thirdparty/src/main/java/pers/xiaomuma/framework/thirdparty/oss/OSSClient.java
@@ -1,6 +1,8 @@
 package pers.xiaomuma.framework.thirdparty.oss;
 
 import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
 
 
 public class OSSClient {
@@ -50,6 +52,30 @@ public class OSSClient {
 
     public boolean expire(String bucket, String filename, int day) {
         return clientAdapter.expire(bucket, filename, day);
+    }
+
+    public String fetchMultipartUploadId(String filename) {
+        return this.fetchMultipartUploadId(clientConfig.getDefaultBucket(), filename);
+    }
+
+    public String fetchMultipartUploadId(String bucket, String filename) {
+        return clientAdapter.fetchMultipartUploadId(bucket, filename);
+    }
+
+    public Map<String, Object> uploadMultipart(String filename, String uploadId, byte[] data, Integer index) {
+        return this.uploadMultipart(clientConfig.getDefaultBucket(), filename, uploadId, data, index);
+    }
+
+    public Map<String, Object> uploadMultipart(String bucket, String filename, String uploadId, byte[] data, Integer index) {
+        return clientAdapter.uploadMultipart(bucket, filename, uploadId, data, index);
+    }
+
+    public String completeUploadMultipart(String filename, String uploadId, List<Map<String, Object>> parts) {
+        return this.completeUploadMultipart(clientConfig.getDefaultBucket(), filename, uploadId, parts);
+    }
+
+    public String completeUploadMultipart(String bucket, String filename, String uploadId, List<Map<String, Object>> parts) {
+        return clientAdapter.completeUploadMultipart(bucket, filename, uploadId, parts);
     }
 
     public static class OSSClientConfig {
