bafda8ef2be66748d6dbcf181bf8691c72ef51f8
xhy <xiehongyan@mail.com>
2023-05-24 11:08:33 +0800

    feat smsbao

---
+++ web-base-thirdparty/src/main/java/pers/xiaomuma/framework/thirdparty/validatecode/smsbao/SmsBaoValidateProperties.java
@@ -0,0 +1,24 @@
+package pers.xiaomuma.framework.thirdparty.validatecode.smsbao;
+
+public class SmsBaoValidateProperties {
+
+    private String username;
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
