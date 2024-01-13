6aae4f21bff8aaa05a6dafa7657e6d66ceef94e2
18146695070 <m18870885528@163.com>
2024-01-13 19:20:33 +0800

    feat 升级swagger3.0 加入knife4j

--- web-base-web/src/main/java/pers/xiaomuma/framework/web/config/SwaggerConfiguration.java
+++ web-base-web/src/main/java/pers/xiaomuma/framework/web/config/SwaggerConfiguration.java
@@ -1,26 +1,30 @@
 package pers.xiaomuma.framework.web.config;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pers.xiaomuma.framework.core.global.ApplicationConstant;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+
+
 @Configuration
-@EnableSwagger2
+@EnableOpenApi
 public class SwaggerConfiguration {
 
 	@Bean
 	public Docket docket(ApplicationConstant applicationConstant) {
 		if (applicationConstant.isProdProfile()) {
-			return new Docket(DocumentationType.SWAGGER_2).enable(false);
+			return new Docket(DocumentationType.OAS_30).enable(false);
 		} else {
-			return new Docket(DocumentationType.SWAGGER_2)
+			return new Docket(DocumentationType.OAS_30)
 					.directModelSubstitute(LocalDate.class, java.sql.Date.class)
 					.directModelSubstitute(LocalDateTime.class, java.util.Date.class)
 					.select()
@@ -37,5 +41,20 @@ public class SwaggerConfiguration {
 					.build();
 		}
 	}
+
+	@Bean
+	public ApiInfo docInfo() {
+		Contact contact = new Contact("xxiaomuma","xiaomuma.com", "m18870885528@163.com");
+		return new ApiInfo(
+				"web-base",
+				"Api文档描述",
+				"1.0.0",
+				"xiaomuma.com",
+				contact,
+				"Apache 2.0",
+				"http://www.apache.org/licenses/LICENSE-2.0",
+				Lists.newArrayList());
+	}
+
 
 }
